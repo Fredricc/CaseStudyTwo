7998516a83f5483f5454abbaef7ad783241b04dc
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-09-24 10:32:49 +0300

    Dictionary example

--- CaseStudyTwo/Program.cs
+++ CaseStudyTwo/Program.cs
@@ -176,12 +176,25 @@ namespace CaseStudyTwo
             //    Console.Write(i + " ");  // 10  15
             //Console.Write("\nCount: " + q.Count);  // 2
 
-            Queue<string> q = new Queue<string>();
-            q.Enqueue("A");
-            q.Enqueue("B");
-            q.Enqueue("C");
-            foreach (string s in q)
-                Console.Write(s + " ");
+            //Queue<string> q = new Queue<string>();
+            //q.Enqueue("A");
+            //q.Enqueue("B");
+            //q.Enqueue("C");
+            //foreach (string s in q)
+            //    Console.Write(s + " ");
+
+            Dictionary<string, int> d = new Dictionary<string, int>();
+            d.Add("Uno", 1);
+            d.Add("One", 1);
+            d.Add("Dos", 2);
+            d.Add("Deux", 2);
+            d.Remove("One");  // Remove key-value pair One, 1
+            d.Remove("Dos");  // Remove key-value pair Dos, 2
+
+            Console.WriteLine("Dictionary: ");
+            foreach (string s in d.Keys)
+                Console.WriteLine(s + ": " + d[s]);  // Uno: 1  Deux: 2
+            Console.WriteLine("\nCount: {0}", d.Count); // 2 
         }
 
         static void PrintBarr(string name, BitArray ba)
